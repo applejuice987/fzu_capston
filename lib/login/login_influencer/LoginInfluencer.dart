65f7eb99240a61897702829cc9ffae1e14f197be
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-09-13 17:31:48 +0900

    로그인 화면 틀 구축2

--- lib/login/login_influencer/LoginInfluencer.dart
+++ lib/login/login_influencer/LoginInfluencer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:fzu/login/MainLoginScreen.dart';
+import 'package:fzu/login/FindAccount.dart';
 import 'package:fzu/login/login_influencer/SignUpInfluencer.dart';
 import 'package:fzu/main.dart';
 
@@ -15,21 +15,69 @@ class LoginInfluencer extends StatefulWidget {
 }
 
 class _LoginInfluencerState extends State<LoginInfluencer> {
+  final in_email_controller = TextEditingController();
+  final in_pw_controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: const EdgeInsets.fromLTRB(30, 0, 30, 30),
           alignment: Alignment.center,
           height: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget> [
-              TextButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpInfluencer()));
-              }, child: Text("회원가입")),
-              TextButton(onPressed: (){
-                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const MyApp()),(Route<dynamic> route) => false);
-              }, child: Text("메인으로 넘어가기")),
+              const Text("인플루언서님 안녕하세요!",style: TextStyle(fontSize: 25),),
+              Column(
+                children: <Widget> [
+                  TextField(
+                    controller: in_email_controller,
+                    decoration: const InputDecoration(
+                      labelText: "이메일",
+                    ),
+                  ),
+                  TextField(
+                    controller: in_pw_controller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "비밀번호",
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 40,
+                    child: ElevatedButton(onPressed: () {
+                      //TODO!! 파이어베이스 연동하고 로그인 넣어야함.;
+                    }, child: const Text("로그인")),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(height: 30,
+                          child: TextButton(onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpInfluencer()));
+                          }, child: const Text("회원가입")),
+                        ),
+                        SizedBox(height: 30,
+                          child: TextButton(onPressed: (){
+                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const MyApp()),(Route<dynamic> route) => false);
+                          }, child: const Text("메인으로 넘어가기")),
+                        ),
+                        SizedBox(height: 30,
+                          child: TextButton(onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const FindAccount()));
+                          }, child: const Text("비밀번호를 잊으셨나요?")),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           )),
     );
--- lib/login/login_sponsor/LoginSponsor.dart
+++ lib/login/login_sponsor/LoginSponsor.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fzu/login/FindAccount.dart';
+import 'package:fzu/login/login_sponsor/SignUpSponsor.dart';
+import 'package:fzu/main.dart';
 
 //TODO!! 여기서 광고주 로그인과 회원가입을 실행할 수 있음.
 
@@ -12,12 +15,98 @@ class LoginSponsor extends StatefulWidget {
 }
 
 class _LoginSponsorState extends State<LoginSponsor> {
+  final sp_email_controller = TextEditingController();
+  final sp_pw_controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+          margin: const EdgeInsets.fromLTRB(30, 0, 30, 30),
           alignment: Alignment.center,
-          child: Text("스폰서 로그인")),
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              const Text(
+                "광고주님 안녕하세요!",
+                style: TextStyle(fontSize: 25),
+              ),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    controller: sp_email_controller,
+                    decoration: const InputDecoration(
+                      labelText: "이메일",
+                    ),
+                  ),
+                  TextField(
+                    controller: sp_pw_controller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "비밀번호",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 40,
+                    child: ElevatedButton(
+                        onPressed: () {
+                         //TODO!! 파이어베이스 연동하고 로그인 알고리즘 넣어야함.
+                          },
+                        child: const Text("로그인")),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                          child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignUpSponsor()));
+                              },
+                              child: const Text("회원가입")),
+                        ),
+                        SizedBox(
+                          height: 30,
+                          child: TextButton(
+                              onPressed: () {
+                                Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const MyApp()),
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: const Text("메인으로 넘어가기")),
+                        ),
+                        SizedBox(
+                          height: 30,
+                          child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const FindAccount()));
+                              },
+                              child: const Text("비밀번호를 잊으셨나요?")),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
