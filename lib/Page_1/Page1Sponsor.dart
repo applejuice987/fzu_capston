3e4aecbdb1ecf37598522e19a68df1757d98b83e
seung ho <316s5615@naver.com>
2022-09-18 21:57:05 +0900

    스폰서용 이미지 삽입

--- lib/Page_1/Page1Sponsor.dart
+++ lib/Page_1/Page1Sponsor.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:fzu/Page_1/influItem.dart';
+
+
+
+
+
+
+
+
+
+final influItem = {
+  "list": [
+    {"image": "images/lee.jpg", "name": "이정재"},
+    {"image": "images/park.jpg", "name": "박은빈"},
+    {"image": "images/kim.jpg", "name": "김연아"},
+    {"image": "images/son.jpg", "name": "손흥민"}
+  ]
+};
+influList? InfluList;
+
+
 
 //TODO!! 로그인 한 사람이 광고주 일 경우, 이 화면 출력
 
@@ -9,94 +31,41 @@ class Page1Sponsor extends StatefulWidget {
 
   @override
   State<Page1Sponsor> createState() => _Page1SponsorState();
-
 }
 
 class _Page1SponsorState extends State<Page1Sponsor> {
+  get bottomNavigationBar => null;
+
   @override
   Widget build(BuildContext context) {
+    InfluList = influList?.fromJson(influItem);
+
     return Scaffold(
       body: GestureDetector(
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-            height: 200,
-            alignment: AlignmentDirectional.center,
-            child: Text('광고모델 모집하기', style: Theme
-                .of(context)
-                .textTheme
-                .headline4
-            ),
-            ),
-             Container(
-               margin: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-
-               alignment: AlignmentDirectional.topEnd
-             ,child: CircleAvatar(
-              radius: 10,
-              child: CupertinoButton(
-                onPressed: () {},
-                child: const Icon(CupertinoIcons.photo_camera_solid,
-                  size: 30,
-                  color: Colors.white,
-                ),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                alignment: AlignmentDirectional.center,
+                child: Text('광고모델 모집하기',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4),
               ),
-            ),
-             ),
-            Center(
-              child: CircleAvatar(
-                radius: 40,
-                child: CupertinoButton(
-                  onPressed: () {},
-                  child: const Icon(CupertinoIcons.photo_camera_solid,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            Text('이름', style: Theme
-                .of(context)
-                .textTheme
-                .subtitle1),
-            Text('채널명', style: Theme
-                .of(context)
-                .textTheme
-                .subtitle1),
-            Text('구독자수', style: Theme
-                .of(context)
-                .textTheme
-                .subtitle1),
-            Text('주요 플랫폼', style: Theme
-                .of(context)
-                .textTheme
-                .subtitle1)
 
 
-          ],
+              Image.asset(InfluList!.list!.elementAt(0).image!),  //influList 의 0번째 요소의 image
+              Text(InfluList!.list!.elementAt(0).name!),  //influList 의 0번째 요소의 name
+
+            ]
         ),
       ),
-      bottomNavigationBar: SafeArea(
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                textStyle: Theme
-                    .of(context)
-                    .textTheme
-                    .subtitle1
-            ),
-            child: Text('매칭하기'),
-
-          )
-
-
-
-    ));
+    );
   }
 
 }
