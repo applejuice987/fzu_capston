985aa63cc928ca7db3fd9a83d63576b65d7f0eaf
applejuice987 <wjdalstn8443@naver.com>
2022-09-21 00:12:37 +0900

    채팅목록 탭시 상세채팅방으로 넘어가기, 길게눌렀을떄 다이얼로그 띄우기

--- lib/Page_3/Page3.dart
+++ lib/Page_3/Page3.dart
@@ -1,6 +1,9 @@
+
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fzu/Page_3/Page3Detail.dart';
 
 //TODO!! 로그인의 관계 없이 같은 화면 출력
 //TODO!! 모든 채팅창 출력
@@ -14,8 +17,42 @@ class Page3 extends StatefulWidget {
 }
 
 class _Page3State extends State<Page3> {
-  final List<String> entries = <String>['A', 'N','tt','rr','A', 'N','tt','rr','A', 'N','tt','rr','A', 'N','tt','rr'];
-  final List<String> en = <String>['B', 'C','g','rr','A', 'N','tt','rr','A', 'N','tt','rr','A', 'N','tt','rr'];
+  final List<String> entries = <String>[
+    'A',
+    'N',
+    'tt',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr'
+  ];
+  final List<String> en = <String>[
+    'B',
+    'C',
+    'g',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr',
+    'A',
+    'N',
+    'tt',
+    'rr'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,38 +63,56 @@ class _Page3State extends State<Page3> {
             itemCount: en.length,
             itemBuilder: (BuildContext context, int index) {
               return ListTile(
-                        width: MediaQuery.of(context).size.width,
-                        child: Row(children: [
-                          Container(
-                            height:100,
-                              margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                              child: Placeholder(
-                                  fallbackHeight: 100, fallbackWidth: 100)),
-                          Column(children: [
-                            Container(child: Text("${en[index]}")),
-                            Container(child: Text("${entries[index]}")),
-                          ])
-                        ,
-                        ]),
+                title: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(children: [
+                    Container(
+                        height: 100,
+                        margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        child: Placeholder(
+                            fallbackHeight: 100, fallbackWidth: 100)),
+                    Column(children: [
+                      Container(child: Text("${en[index]}")),
+                      Container(child: Text("${entries[index]}")),
+                    ]),
+                  ]),
+
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => Page3Detail()));
+                },
+                onLongPress: ()=> showDialog(context: context,
+                    barrierDismissible: false,
+                    builder:(BuildContext context){
+                      return AlertDialog(
+                        title:Text('다이얼로그 메시지'),
+                        content:SingleChildScrollView(
+                          child:ListBody(
+                            children: <Widget>[
+                              Text('testleft'),
+                              Text('testright')
+
+                            ],
+                          )
+                        ),
+                        actions: <Widget>[
+                          TextButton(onPressed:(){
+                            Navigator.of(context).pop();
+                          }, child: Text('ok')),
+                          TextButton(onPressed:(){
+                            Navigator.of(context).pop();
+                          }, child: Text('cancel'))
+                        ],
+
 
                       );
 
+                    }),
+              );
             }));
   }
-
-  Scaffold buildScaffold() {
-    return Scaffold(
-      body: Column(children: <Widget>[
-        Row(children: [
-          Container(
-              width: 100,
-              child: Placeholder(fallbackHeight: 100, fallbackWidth: 100)),
-          Column(children: [
-            Container(child: const Text('이름 및 별명')),
-            Container(child: const Text('마지막채팅'))
-          ])
-        ])
-      ]),
-    );
-  }
 }
+
+
