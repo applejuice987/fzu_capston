fdb64feb5b928a63ef9922ee29baf2ecb868b521
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-10-28 15:15:55 +0900

    도훈이형꺼 실시간으로 새로고침 되게 함

--- lib/Page_2/Page2Sponsor.dart
+++ lib/Page_2/Page2Sponsor.dart
@@ -3,26 +3,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fzu/MySharedPreferences.dart';
 import 'package:fzu/Page_2/Page2Sponsor2.dart';
 import 'package:fzu/Page_2/Page2Sponsor3.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 //TODO!! 로그인 한 사람이 스폰서 일 경우, 이 화면 출력
 
 class Page2Sponsor extends StatefulWidget {
-  const Page2Sponsor({Key? key}) : super(key: key);
-
+  const Page2Sponsor({Key? key, required this.list}) : super(key: key);
+  final List<String> list;
   @override
   State<Page2Sponsor> createState() => _Page2SponsorState();
-}
-void getdata1() {
-  var db = FirebaseFirestore.instance;
-  db.collection("sponsor").get().then((value) {
-    for (var doc in value.docs) {
-      String title = doc["title"];
-      String content = doc["content"];
-    }
-  });
 }
 
 class _Page2SponsorState extends State<Page2Sponsor> {
@@ -33,45 +26,26 @@ class _Page2SponsorState extends State<Page2Sponsor> {
   //CollectionReference sponsor = FirebaseFirestore.instance.collection('sponsor');
 
   FirebaseAuth auth = FirebaseAuth.instance;
-  late String docId = auth.currentUser!.email.toString();
-  //CollectionReference sponsor = FirebaseFirestore.instance.collection('sponsor');
-
-  Stream collectionStream = FirebaseFirestore.instance.collection('sponsor').snapshots();
-  Stream documentStream = FirebaseFirestore.instance.collection('sponsor').doc('docId').collection('recruit').snapshots();
-  //var documentSnapshot = sponsor.doc('docId').get();
-
-  //getData() async {
-  //var result = await firestore.collection("sponsor").doc("docId").get().then((value){
-  //for(var doc in ){
+  // getData() async {
+  // var result = await firestore.collection("sponsor").doc("docId").get().then((value){
+  // for(var doc in ){
   // String title = doc["title"];
   // String content = doc["content"];
-
+  //
   // }
   //  });
-  //return result;
+  // return result;
   // }
-
-
-  String _currentUser = '';
-  List<String> _titleList = [];
 
   void initState() {
     super.initState();
-    var db = FirebaseFirestore.instance;
-
-    _currentUser = FirebaseAuth.instance.currentUser!.email.toString();
-    db.collection("sponsor").doc('dhgns3926@daum.net').collection('recruit').get().then((value) {
-      for (var doc in value.docs) {
-        String title = doc["title"];
-        String content = doc["content"];
-        _titleList.add(doc['title'].toString());
-      }
-    });
   }
 
 
   @override
   Widget build(BuildContext context) {
+   // var _titleList = MySharedPreferences.instance.getStringList('albamon');
+    List<String> _titleList = widget.list;
     return MaterialApp(
       home: Scaffold(
         body: CustomScrollView(
@@ -81,7 +55,6 @@ class _Page2SponsorState extends State<Page2Sponsor> {
             //floating: true,
             //flexibleSpace: Placeholder(),
             // expandedHeight: 100,
-
             // ),
             SliverToBoxAdapter(
 
@@ -114,46 +87,22 @@ class _Page2SponsorState extends State<Page2Sponsor> {
                   ),
                 )
             ),
-
-
             SliverList(
               delegate: SliverChildBuilderDelegate(
-                    (context, index) => ListTile(title:Text(_titleList[index]),
+                    (context, index) => _titleList.isEmpty ? Text("아무것도 없음") : ListTile(title:Text(_titleList[index]),
                   onTap: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
                             builder: (context) => const Page2Sponsor2()));
                   },),
-                childCount: 10,
+                childCount: _titleList.length,
               ),
             ),
             // StreamBuilder<QuerySnapshot>(
-            // stream: firestore.collection('sponsor').snapshots(),
-
-            // builder: (context, snapshot) {
-            //  if (snapshot.connectionState == ConnectionState.waiting){
-            //   return CircularProgressIndicator();
-            //  }
-            //  return ListView.builder(
-            //   itemCount: snapshot.data!.documents.length,
-            //  itemBuilder: (ctx, index) => Container(
-            //    padding: EdgeInsets.all(8),
-            //    child: Text(snapshot.data.documents[index]['text']),
-            //   ),
-            //  );
-            //  }
-            //  ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
